f21dabfc7c3bb5c46e9597ffe15cd09fd06ac08c
egaranda <egaranda@e-externas.enaire.es>
2023-01-16 18:46:37 +0100

    Commit cambios

---
+++ src/main/java/com/unisys/miapp/App.java
@@ -0,0 +1,26 @@
+package com.unisys.miapp;
+
+public class App {
+
+    /**
+     * Clase para la suma de importes
+     * @autor Toyo
+     *
+     * @version 1.0
+     */
+    public static void main(String[] args) {
+
+        int numero;
+        short marianico = 10;
+        long gasol = 1000;
+        boolean independencia;
+        double presupuesto;
+        float longuitud;
+        int suma;
+
+        // sumamos los importes
+        suma = (int)gasol + marianico;
+
+        System.out.println("Suma del corto y el largo: "+suma);
+    }
+}
